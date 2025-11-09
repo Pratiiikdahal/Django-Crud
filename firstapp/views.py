47da993168bb84c7e31bd593c956657cdf69f92f
Pratik Dahal <notpratiiiikdahal@gmail.com>
2025-11-09 13:57:42 +0545

    crud completed

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from firstapp.models import *
 from firstapp.forms import StudentForms
 # Create your views here.
 
 def index(request,id=None):
-    student_values=students.objects.all()
-    form=StudentForms()
+    if id:
+        students_updated=get_object_or_404(students,id=id)
+        form=StudentForms(request.POST or None,instance=students_updated)
+    else:
+        form=StudentForms(request.POST or None)
+        
+   
+    # form=StudentForms()
     if request.method=='POST':
-        form=StudentForms(request.POST)
+        if "delete" in request.POST:
+            to_be_deleted=get_object_or_404(students,id=request.POST['delete'])
+            to_be_deleted.delete()
+            return redirect('index-view')
+        
+        form=form
         if form.is_valid():
             form.save()
             return redirect('index-view')
+        
+    student_values=students.objects.all()
     context={'student':student_values,
              'form':form}
     return render(request,'firstapp/index.html',context=context)
